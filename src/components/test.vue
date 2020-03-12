6236ff4629890ef34d2d09192691afd90ffe3096
zky <597768024@qq.com>
2020-03-12 18:53:42 +0800

    增加mock模块

---
+++ src/components/test.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <button @click="login">login</button>
+        <button @click="list">list</button>
+        <button @click="post">post</button>
+        <button @click="remove">remove</button>
+    </div>
+    <!--App -->
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  methods: {
+    login () {
+      axios.get('/api/login').then(response => {
+        if (response.data) {
+          console.log(response.data)
+          alert(response.data.success + ',' + response.data.message)
+        }
+      })
+    },
+    list () {
+      axios.get('/api/list').then(response => {
+        if (response.data) {
+          console.log(response.data)
+          const list1 = response.data[0].id + ',' + response.data[0].sex + ',' + response.data[0].username
+          const list2 = response.data[1].id + ',' + response.data[1].sex + ',' + response.data[1].username
+          alert(list1 + '\n' + list2)
+        }
+      })
+    },
+    post () {
+      axios.post('/api/post').then(response => {
+        if (response.data) {
+          console.log(response.data)
+          alert(response.data.status + ',' + response.data.code)
+        }
+      })
+    },
+    remove () {
+      axios.delete('/api/remove').then(response => {
+        if (response.data) {
+          console.log(response.data)
+          alert(response.data.status + ',' + response.data.message)
+        }
+      })
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Test from '../components/test.vue'
 import Login from '../components/login.vue'
 import Register from '../components/register.vue'
 import Index from '../components/index.vue'
@@ -7,6 +8,7 @@ import Index from '../components/index.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/test', component: Test },
   // 路由重定向，/跳转到登录页面
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
